ac88ed020bf79f8d04d1c661e9432cfc610266f1
vitoria-vercosa <vitoriavercosa@alu.ufc.br>
2023-02-07 23:08:11 -0300

    Adding Unit Test project

---
+++ FT-ProviderSys.UnitTest/ProviderUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using FT_ProviderSys.Controllers;
+using FT_ProviderSys.Services.Interfaces;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FT_ProviderSys.UnitTest
+{
+    public class ProviderUnitTest
+    {
+        private readonly ProviderController _providerController;
+        private readonly Mock<IProviderService> _mockProviderService;
+
+        public ProviderUnitTest()
+        {
+            _mockProviderService = new Mock<IProviderService>();
+            _providerController = new ProviderController(_mockProviderService.Object);
+        }
+
+        [Fact(DisplayName = "GetAll Should Return 200Ok")]
+        [Trait("Provider","GetAll")]
+        public async Task GetAll_ShouldReturn200Ok()
+        {
+            // Act
+            var response = await _providerController.GetAll();
+            var statusCode = (response.Result as ObjectResult).StatusCode;
+
+            // Assert
+            Assert.Equal(200, statusCode);
+        }
+    }
+}
---
+++ FT-ProviderSys.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace FT_ProviderSys.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
